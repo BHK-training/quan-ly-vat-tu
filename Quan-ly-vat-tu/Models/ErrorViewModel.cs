4a579ec11edd9efb1c8def59c0ebcdfdf8dfdfde
daohocvan <daohocvan@users.noreply.github.com>
2020-08-25 10:20:47 +0700

    Add project files.

---
+++ Quan-ly-vat-tu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quan_ly_vat_tu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
